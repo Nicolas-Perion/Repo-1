fdf7023c2fe4879fe95baa128cf6a81da22c7222
Nicolas-Perion <163454214+Nicolas-Perion@users.noreply.github.com>
2026-01-22 13:57:35 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from typing import Union
-from fastapi import FastAPI
-from surface_score import compute_surface
-
-app = FastAPI()
-
-@app.get("/surface")
-def surface():
-    return {"surface": compute_surface()}
